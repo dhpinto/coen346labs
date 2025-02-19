80fce9929fe45ce262859c54b4dea69d0e59a881
dhpinto <danielp5504@gmail.com>
2025-02-19 11:15:53 -0500

    Capitalized comments

--- main.cpp
+++ main.cpp
@@ -14,7 +14,7 @@ int globalID = 0;
 void mergeSortInAThread(std::vector<int> &arr, int left, int right, int threadID);
 void mergeSort(std::vector<int> &arr, int left, int right);
 
-// here we are creating two subarrays, one left and one right
+// Creating two subarrays, one left and one right
 void merge(std::vector<int> &arr, int left, int mid, int right)
 {
 	int arrLeft = mid - left + 1;
@@ -33,7 +33,7 @@ void merge(std::vector<int> &arr, int left, int mid, int right)
 	{
 		tempRight[j] = arr[mid + 1 + j];
 	}
-	// initializing to merge
+	// Initializing to merge
 	int i = 0;
 	int j = 0;
 	int k = left;
@@ -51,18 +51,18 @@ void merge(std::vector<int> &arr, int left, int mid, int right)
 	}
 	// For remaining elements in non-empty subarray
 
-	// elements from tempLeft[]
+	// Elements from tempLeft[]
 	while (i < arrLeft)
 	{
 		arr[k++] = tempLeft[i++];
 	}
-	// elements from tempRight
+	// Elements from tempRight
 	while (j < arrRight)
 	{
 		arr[k++] = tempRight[j++];
 	}
 }
-std::string printVector(std::vector<int> &arr, int left, int right) // prepare the message to cout in one shot
+std::string printVector(std::vector<int> &arr, int left, int right) // Prepare the message to cout in one shot
 {
 	std::string printResult;
 	for (int i = left; i <= right; i++)
@@ -72,17 +72,17 @@ std::string printVector(std::vector<int> &arr, int left, int right) // prepare t
 	return printResult;
 }
 
-void mergeSortInAThread(std::vector<int> &arr, int left, int right, int threadID) // this function is an intermediate function called wrapping
+void mergeSortInAThread(std::vector<int> &arr, int left, int right, int threadID) // This function is an intermediate function called wrapping
 {
 	{
-		std::lock_guard<std::mutex> lock(mtx); // used so cout from threads working simultaneously do not overlap
+		std::lock_guard<std::mutex> lock(mtx); // Used so cout from threads working simultaneously do not overlap
 		std::cout << "Thread " << std::bitset<4>(threadID).to_string() << " started" << std::endl;
 	}
-	mergeSort(arr, left, right); // the parameter function call gets forward
+	mergeSort(arr, left, right); // The parameter function call gets forward
 	{
 		std::lock_guard<std::mutex> lock(mtx);
 		std::cout << "Thread " << std::bitset<4>(threadID).to_string() << " finished:  " << printVector(arr, left, right) << std::endl;
-		// make one big string so it can cout in one shot
+		// Make one big string so it can cout in one shot
 	}
 }
 
@@ -94,18 +94,18 @@ void mergeSort(std::vector<int> &arr, int left, int right)
 	int mid = left + (right - left) / 2;
 
 	{
-		std::lock_guard<std::mutex> lock(id_mtx); // ensures that only one thread can access the globalID at a time
+		std::lock_guard<std::mutex> lock(id_mtx); // Ensures that only one thread can access the globalID at a time
 		globalID++;
 	}
-	std::thread lm(mergeSortInAThread, std::ref(arr), left, mid, globalID); // recursion 1 (left half), pass by reference
+	std::thread lm(mergeSortInAThread, std::ref(arr), left, mid, globalID); // Recursion 1 (left half), pass by reference
 	{
 		std::lock_guard<std::mutex> lock(id_mtx);
 		globalID++;
 	}
-	std::thread rm(mergeSortInAThread, std::ref(arr), mid + 1, right, globalID); // recursion 2 (right half)
+	std::thread rm(mergeSortInAThread, std::ref(arr), mid + 1, right, globalID); // Recursion 2 (right half)
 
-	lm.join(); // waiting for left thread to finish
-	rm.join(); // waiting for right thread to finish
+	lm.join(); // Waiting for left thread to finish
+	rm.join(); // Waiting for right thread to finish
 
 	merge(arr, left, mid, right);
 }
@@ -125,15 +125,15 @@ int main()
 	std::vector<int> arr;
 
 	std::ofstream out("out.txt");
-	//   std::streambuf* coutbuf = std::cout.rdbuf(); //save old buf
-	std::cout.rdbuf(out.rdbuf()); // redirect std::cout to out.txt!
+
+	std::cout.rdbuf(out.rdbuf()); // Redirect std::cout to out.txt
 
 	std::string inputMyText;				 // Create a text string, which is used to output the text file
-	std::ifstream ReadMyInputFile("in.txt"); // put cursor at the first line of the text
+	std::ifstream ReadMyInputFile("in.txt"); // Put cursor at the first line of the text
 
-	while (std::getline(ReadMyInputFile, inputMyText)) // get the first line of the cursor and put it in inputMyTest string
+	while (std::getline(ReadMyInputFile, inputMyText)) // Get the first line of the cursor and put it in inputMyTest string
 	{
-		arr.push_back(stoi(inputMyText)); // pushback inputMytext words into the array called arr
+		arr.push_back(stoi(inputMyText)); // Pushback inputMytext words into the array called arr
 	}
 	int n = arr.size();
 	ReadMyInputFile.close();
